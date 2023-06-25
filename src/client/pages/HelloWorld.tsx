b74b12f173f54c9d1e2861889469eb898d91686a
Dmytro_Kostyushko <dmytro.kostyushko@botscrew.com>
2023-06-26 00:43:38 +0300

    added charts to the example

--- src/client/pages/HelloWorld.tsx
+++ src/client/pages/HelloWorld.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import { getInitialPropsFromRequestBody } from '../utilities/NextJsUtilities';
 import { NextPage } from 'next';
+import Chart from 'chart.js/auto';
+
+
 
 type HelloWorldTemplateProps = {
   name: string
@@ -11,9 +15,32 @@ const HelloWorld: NextPage<HelloWorldTemplateProps> = props => {
 
   const initialProps = props as HelloWorldTemplateProps;
 
+  useEffect(() => {
+    const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1,
+        }],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }, []);
+
   return (
     <div id='root'>
       <h1>Hello {initialProps.name}</h1>
+      <canvas id='myChart'></canvas>
     </div>
   );
 };
